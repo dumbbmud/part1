6199e053ed50b8d54222ba2f16c542cfa195eec6
dumbbmud <ayeshamani63@gmail.com>
2025-04-24 20:14:46 -0500

    made changes in accordance to part 1.12

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -50,6 +50,7 @@
 
 import { useState } from 'react'
 
+
 const Display = ({text}) => <><h1>{text}</h1></>
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
@@ -88,25 +89,27 @@ const Statistics = ({good, bad, neutral}) => {
 
 }
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(() => Math.floor(Math.random() * anecdotes.length))
 
-  const goodIncrement = () => setGood(good+1)
-  const neutralIncrement = () => setNeutral(neutral+1)
-  const badIncrement = () => setBad(bad+1)
 
 
   return (
     <div>
-      <Display text="give feedback" />
-      <Button onClick={goodIncrement} text="good"/>
-      <Button onClick={neutralIncrement} text="neutral"/>
-      <Button onClick={badIncrement} text="bad"/>
-      <Display text={"statistics"} />
-      <Statistics good={good} bad={bad} neutral={neutral}/>
-      
+      {anecdotes[selected]}
+      <br/>
+      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
+      <br/>
     </div>
   )
 }
